06b1a7b65530ee1c236a099c3667482d10ab20b9
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-07-29 23:35:34 -0300

    structuring GameOver.JSX and creating its style

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,10 +1,18 @@
+import { useContext } from "react"; // pegar a pontuação do usuario!
+import { QuizContext } from "../../context/quiz"; // importando o contexto
+
+import WellDone from "../../img/welldone.svg";
 import "./GameOver.css";
 
 
 const GameOver = () => {
   return (
-    <div>
-      <h2>Game Over</h2>
+    <div id="gameover">
+      <h2>Fim de Jogo!</h2>
+      <p>Sua pontuação: x </p>
+      <p>Você acertou 2 de 5 perguntas!</p>
+      <img src={WellDone} alt="fim de jogo!" />
+      <button>Reiniciar</button>
     </div>
   )
 }
