9b0d440ca81f525d679329f480845d892e06c80f
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-07-30 20:42:15 -0300

    added answer styles

--- src/components/OptionComponent/OptionComponent.jsx
+++ src/components/OptionComponent/OptionComponent.jsx
@@ -7,8 +7,12 @@ const OptionComponent = ({option,selectOption,answer }) => {
     const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div className="option" onClick={()=> selectOption()}>
+    <div className={`option 
+
+      ${quizState.answerSelected && option === answer ? 'correct' : ""} 
+      ${quizState.answerSelected && option !== answer ? 'wrong' : ""}`} onClick={()=> selectOption()}>
       <p>{option}</p>
+
     </div>
   )
 }
